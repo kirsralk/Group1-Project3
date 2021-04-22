339c0032d278afa675db913aa5b2c12cb48f0a44
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-04-21 19:46:32 -0500

    added Navbar component

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Crypto Chat
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/about"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Logout
+            </Link>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
