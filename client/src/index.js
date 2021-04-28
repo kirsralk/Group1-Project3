bbfd60ba437624279543d4c659fbf4bf0320dd93
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-04-28 11:07:32 -0500

    login page added

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-mdrmoj72.us.auth0.com"
+    clientId="1MNv0fwq3DCgg74cuMZsy5v2SKRo9cqq"
+    redirectUri="http://localhost:3000/main"
+    // {window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
