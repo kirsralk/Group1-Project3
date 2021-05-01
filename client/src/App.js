2ed6eb7d6bdefbb82273b2cfaf8ded20e6ea3353
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-05-01 15:04:04 -0500

    Navbar updates | render coin logos in Ticker

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar1 from "./components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from "./pages/Main"
-import Login from "./pages/Login"
+import Main from "./pages/Main";
+import Login from "./pages/Login";
 // import { Table } from "react-bootstrap";
 
 
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Navbar />
+        <Navbar1 />
           <Route exact path="/" component={Login} />
           <Route exact path="/main" component={Main}/>
       </Router>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,36 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 import LogoutButton from "../Logout";
-// import { useAuth0 } from "@auth0/auth0-react";
-// import User from "../User";
+import { useAuth0 } from "@auth0/auth0-react";
+import User from "../User";
+import { Navbar } from "react-bootstrap";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
+function Navbar1() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Crypto Chat
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            {/* <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Logout
-            </Link> */}
-            <LogoutButton />
-          </li>
-
-        </ul>
-      </div>
-    </nav>
+    isAuthenticated && (
+      <Navbar variant="dark">
+        <Navbar.Brand href="#home">Crypto Chat</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>Logged in as: <b>{user.name}</b> &nbsp;</Navbar.Text>
+          <LogoutButton />
+        </Navbar.Collapse>
+      </Navbar>
+    )
   );
 }
 
-export default Navbar;
+export default Navbar1;
