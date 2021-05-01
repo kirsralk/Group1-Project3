33ef955e4ac78ece835d35b783f166437ccde3df
Cody Yuill <codyyuill@hotmail.com>
2021-05-01 11:40:06 -0400

    single posts now retireved from db. moved singlepost and allpost views into their own components so that forum is easier to read. also commented forum so that its not complete gibberish

---
+++ client/src/components/SingleForumPost/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+import API from "../../utils/API";
+
+function SingleForumPost(props) {
+
+
+    return (
+        <>
+            <p>
+                {props.post.title} created by {props.post.user}
+            </p>
+            {props.replies.map((value, index) => {
+                return (
+                    <div key={index}>
+                        <p>{value.user}: </p>
+                        <p>{value.body}</p>;<hr></hr>
+                    </div>
+                );
+            })}
+            <input type="button" value="X" onClick={props.closePost}></input>
+            <form action="/action_page.php" onSubmit={props.submitReply}>
+                <input
+                    type="text"
+                    id="postBody"
+                    name="postBody"
+                    placeholder="Body"
+                ></input>
+                <input type="submit" value="Submit Reply"></input>
+            </form>
+        </>
+    );
+}
+
+export default SingleForumPost;
