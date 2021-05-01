c5154ede71a71214df05176b599a77ae16e78232
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-05-01 11:14:13 -0500

    theme changes to dark | remove unused-vars | add Hero component

--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import "./style.css";
 
 const User = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -13,7 +14,7 @@ const User = () => {
       <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        {/* <p>{user.email}</p> */}
       </div>
     )
   );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
-import logo from "../logo.svg";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import LoginButton from "../components/LoginButton";
 
 
