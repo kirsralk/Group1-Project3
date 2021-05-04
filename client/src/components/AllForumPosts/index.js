698b636a074e07ad7d8f38ed8d702e4d4428b675
Cody Yuill <codyyuill@hotmail.com>
2021-05-03 20:29:41 -0400

    made posts cards, styled reply inputs to match post inputs. made replies vertical instead of horizontal

--- client/src/components/AllForumPosts/index.js
+++ client/src/components/AllForumPosts/index.js
@@ -1,33 +1,56 @@
 import React from "react";
 import "./style.css";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Card, Container, Col } from "react-bootstrap";
 
 function AllForumPosts(props) {
     return (
         <>
             {props.posts.map((value, index) => {
                 return (
-                    <div key={index} onClick={() => props.clickPost(value)}>
-                        <p id={value._id}>
-                            {value.title} created by {value.user}
-                        </p>
-                        <p>replies: {value.replies.length}</p>
-                        <hr></hr>
-                    </div>
+                    <Card
+                        key={index}
+                        onClick={() => props.clickPost(value)}
+                        className="postCards"
+                    >
+                        <Card.Header>{value.title}</Card.Header>
+                        <Card.Body>
+                            <Card.Text>{value.body}</Card.Text>
+                            <Card.Footer>
+                                <Container fluid>
+                                    <Col lg={6}>
+                                        <Card.Subtitle className="mb-2 text-muted postUserText">
+                                            <p>{value.user}</p>
+                                        </Card.Subtitle>
+                                    </Col>
+                                    <Col lg={6}>
+                                        <Card.Subtitle className="mb-2 text-muted">
+                                            <p>{value.replies.length} replies</p>
+                                        </Card.Subtitle>
+                                    </Col>
+                                </Container>
+                            </Card.Footer>
+                        </Card.Body>
+                    </Card>
                 );
             })}
 
             <Form onSubmit={props.submitPost}>
                 <Form.Group id="form">
-                    <Form.Control placeholder="Post Title..." id="postTitle" name="postTitle"/>
+                    <Form.Control
+                        placeholder="Post Title..."
+                        id="postTitle"
+                        name="postTitle"
+                        className="postInputs"
+                    />
                     <Form.Control
                         as="textarea"
                         rows={3}
                         placeholder="Post Body..."
                         id="postBody"
                         name="PostBody"
+                        className="postInputs"
                     />
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" type="submit" id="submitPostBtn">
                         Submit
                     </Button>
                 </Form.Group>
--- client/src/components/SingleForumPost/index.js
+++ client/src/components/SingleForumPost/index.js
@@ -1,32 +1,48 @@
 import React from "react";
 import "./style.css";
+import { Container, Row, Form, Button } from "react-bootstrap";
 
 function SingleForumPost(props) {
-
-
     return (
         <>
-            <p>
-                {props.post.title} created by {props.post.user}
-            </p>
-            {props.replies.map((value, index) => {
-                return (
-                    <div key={index}>
-                        <p>{value.user}: </p>
-                        <p>{value.body}</p>;<hr></hr>
-                    </div>
-                );
-            })}
-            <input type="button" value="X" onClick={props.closePost}></input>
-            <form action="/action_page.php" onSubmit={props.submitReply}>
-                <input
-                    type="text"
-                    id="postBody"
-                    name="postBody"
-                    placeholder="Body"
-                ></input>
-                <input type="submit" value="Submit Reply"></input>
-            </form>
+            <Container fluid>
+                <Row>
+                    <p>
+                        {props.post.title} created by {props.post.user}
+                    </p>
+                    <Button type="button" onClick={props.closePost}>
+                        X
+                    </Button>
+                </Row>
+                <Form onSubmit={props.submitReply}>
+                    <Form.Group id="form">
+                        <Form.Control
+                            as="textarea"
+                            rows={3}
+                            placeholder="Reply..."
+                            id="postBody"
+                            name="PostBody"
+                            className="postInputs"
+                        />
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            id="submitPostBtn"
+                        >
+                            Submit
+                        </Button>
+                    </Form.Group>
+                </Form>
+                {props.replies.map((value, index) => {
+                    return (
+                        <Row key={index}>
+                            <p>{value.user}: </p>
+                            <p>{value.body}</p>
+                            <hr></hr>
+                        </Row>
+                    );
+                })}
+            </Container>
         </>
     );
 }
