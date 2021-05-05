64e2ba8f9876eabac6b833838671b9933d698c4b
Cody Yuill <codyyuill@hotmail.com>
2021-05-05 19:40:09 -0400

    finished single post forum view

--- client/src/components/SingleForumPost/index.js
+++ client/src/components/SingleForumPost/index.js
@@ -1,75 +1,97 @@
 import React from "react";
 import "./style.css";
-import { Container, Row, Form, Button, Col } from "react-bootstrap";
-import { formatDistance, parseISO } from 'date-fns';
-
+import { Container, Row, Form, Button, Col, Card } from "react-bootstrap";
+import { formatDistance, parseISO } from "date-fns";
 
 function SingleForumPost(props) {
     return (
         <Container>
-            <Row>
-                <Col xs={11}>
+            <Card bg="dark">
+                <Container id="singlePostCard">
                     <Row>
-                        <h1 className="float-left">{props.post.title}</h1>
-                    </Row>
-                    <Row>
-                        <p className="float-left">
-                            created by {props.post.user} on {new Date(props.date).toDateString()} 
-                        </p>
-                    </Row>
-                </Col>
-                <Col xs={1}>
-                    <Button
-                        variant="outline-light"
-                        type="button"
-                        onClick={props.closePost}
-                        className="float-right"
-                    >
-                        X
-                    </Button>
-                </Col>
-            </Row>
-            <Row>
-                <p>{props.post.body}</p>
-            </Row>
-            <Row>
-                <Col>
-                    <Form onSubmit={props.submitReply}>
-                        <Form.Group id="form">
-                            <Form.Control
-                                as="textarea"
-                                rows={3}
-                                placeholder="Reply..."
-                                id="postBody"
-                                name="PostBody"
-                                className="replyInput"
-                            />
+                        <Col xs={10} className='postHeader'>
+                            <Row>
+                                <h1 className="float-left">
+                                    {props.post.title}
+                                </h1>
+                            </Row>
+                            <Row>
+                                <p className="float-left">
+                                    by {props.post.user} on{" "}
+                                    {new Date(props.date).toDateString()}
+                                </p>
+                            </Row>
+                        </Col>
+                        <Col xs={1} id='closeBtn'>
                             <Button
-                                variant="primary"
-                                type="submit"
-                                id="submitReplytBtn"
+                                variant="link"
+                                type="button"
+                                onClick={props.closePost}
                                 className="float-right"
                             >
-                                Submit Reply
+                                X
                             </Button>
-                        </Form.Group>
-                    </Form>
-                </Col>
-            </Row>
-            {props.replies.map((value, index) => {
-                return (
-                    <Row key={index}>
-                        <Col xs={2}>
-                            <p className="replyUser">{value.user}: </p>
-                            <p className="replyUser">{formatDistance(parseISO(new Date(value.createdAt).toISOString()), new Date(), { addSuffix: true })}</p>
                         </Col>
-                        <Col xs={10}>
-                            <p className="replyText">{value.body}</p>
-                        </Col>
-                        <hr></hr>
                     </Row>
-                );
-            })}
+                    <Row className='postHeader'>
+                        <p className="postBody">{props.post.body}</p>
+                    </Row>
+                    <Row id="replyForm">
+                        <Col>
+                            <Form onSubmit={props.submitReply}>
+                                <Form.Group id="form">
+                                    <Form.Control
+                                        as="textarea"
+                                        rows={3}
+                                        placeholder="Reply..."
+                                        id="postBody"
+                                        name="PostBody"
+                                        className="replyInput"
+                                    />
+                                    <Button
+                                        variant="primary"
+                                        type="submit"
+                                        id="submitReplytBtn"
+                                        className="float-right"
+                                    >
+                                        Submit Reply
+                                    </Button>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                    </Row>
+                    {props.replies.map((value, index) => {
+                        return (
+                            <Card bg="dark" className="reply">
+                                <Row key={index} noGutters={true}>
+                                    <Col xs={2} className="userInfo">
+                                        <h5 className="replyUserName">
+                                            {value.user} 
+                                        </h5>
+                                        <p className="replyUserDate">
+                                            {formatDistance(
+                                                parseISO(
+                                                    new Date(
+                                                        value.createdAt
+                                                    ).toISOString()
+                                                ),
+                                                new Date(),
+                                                { addSuffix: true }
+                                            )}
+                                        </p>
+                                    </Col>
+                                    <Col xs={10}>
+                                        <p className="replyText">
+                                            {value.body}
+                                        </p>
+                                    </Col>
+                                    <hr></hr>
+                                </Row>
+                            </Card>
+                        );
+                    })}
+                </Container>
+            </Card>
         </Container>
     );
 }
