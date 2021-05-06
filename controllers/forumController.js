87c43926c235b880fa37427de900636602a2e2d4
Cody Yuill <codyyuill@hotmail.com>
2021-05-05 20:33:11 -0400

    posts sorted newest to oldest

--- controllers/forumController.js
+++ controllers/forumController.js
@@ -4,7 +4,7 @@ const db = require("../models");
 module.exports = {
   findAll: function(req, res) {
     db.Post.find({})
-      .sort({ date: -1 })
+      .sort({ createdAt: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
